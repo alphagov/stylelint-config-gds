c5eed634efaee46b6e8ea4032c12b494f88739c5
Kevin Dew <kevindew@me.com>
2020-05-25 19:56:15 +0100

    Add a basic test
    I think the only thing we need to test here is that we've got valid
    rules defined. Since the rules themselves are configuration we shouldn't
    actually test them.

---
+++ test/rules.test.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+
+var cssRules = require('../css')
+var scssRules = require('../scss')
+var stylelint = require('stylelint')
+var goodCss = '.class-name { content: ""; }\n'
+var noErrors = expect.objectContaining({
+  errored: false,
+  results: expect.arrayContaining([
+    expect.objectContaining({
+      deprecations: [],
+      invalidOptionWarnings: [],
+      parseErrors: [],
+      warnings: []
+    })
+  ])
+})
+
+it("css rules don't have errors, warnings or deprecations", function () {
+  var promise = stylelint.lint({ config: cssRules, code: goodCss })
+  return expect(promise).resolves.toEqual(noErrors)
+})
+
+it("scss rules don't have errors, warnings or deprecations", function () {
+  var promise = stylelint.lint({ config: scssRules, code: goodCss })
+  return expect(promise).resolves.toEqual(noErrors)
+})
