fd1f376fecfa64abea93edabe09171ef7cd0da84
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-11-13 17:37:23 +0000

    Remove rules already defined in stylelint-config-standard-scss

--- scss.js
+++ scss.js
@@ -10,15 +10,6 @@ module.exports = {
     // This is disabled for SCSS as it prevents SCSS specific @ rules (such as @if)
     // https://stylelint.io/user-guide/rules/at-rule-no-unknown
     'at-rule-no-unknown': null,
-    // Require a new line after a @else { } statement
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-else-closing-brace-newline-after
-    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
-    // Require a space after an @else {} before the next @else rule
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-else-closing-brace-space-after
-    'scss/at-else-closing-brace-space-after': 'always-intermediate',
-    // Disallow empty lines before an @else statement
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-else-empty-line-before
-    'scss/at-else-empty-line-before': 'never',
     // Only allow @extend with a placeholder
     // https://github.com/kristerkari/stylelint-scss/blob/master/src/rules/at-extend-no-missing-placeholder
     // Originates from: https://github.com/alphagov/govuk-frontend/blob/e248b4027102b2684f592a0501630075bdfa1fab/config/.sass-lint.yml#L242
@@ -31,13 +22,6 @@ module.exports = {
           'may start with an underscore'
       }
     ],
-    // Require a new line character at the termination of a group of @if / @else
-    // definitions
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-if-closing-brace-newline-after
-    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-    // Require a space between an @if {} ending and an @else beginning
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-if-closing-brace-space-after
-    'scss/at-if-closing-brace-space-after': 'always-intermediate',
     // Disallow importing partials with a underscore prefix
     // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-import-no-partial-leading-underscore
     // Originates from: https://github.com/alphagov/govuk-frontend/blob/e248b4027102b2684f592a0501630075bdfa1fab/config/.sass-lint.yml#L46
@@ -46,10 +30,6 @@ module.exports = {
     // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-import-partial-extension
     // Originates from: https://github.com/alphagov/govuk-frontend/blob/e248b4027102b2684f592a0501630075bdfa1fab/config/.sass-lint.yml#L45
     'scss/at-import-partial-extension': 'never',
-    // Disallow parenthesis when including a mixin with no parameters
-    // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-mixin-argumentless-call-parentheses
-    // Originates from: https://github.com/alphagov/govuk-frontend/blob/e248b4027102b2684f592a0501630075bdfa1fab/config/.sass-lint.yml#L48
-    'scss/at-mixin-argumentless-call-parentheses': 'never',
     // https://github.com/kristerkari/stylelint-scss/tree/master/src/rules/at-mixin-pattern
     // Originates from: https://github.com/alphagov/govuk-frontend/blob/e248b4027102b2684f592a0501630075bdfa1fab/config/.sass-lint.yml#L125
     'scss/at-mixin-pattern': [
